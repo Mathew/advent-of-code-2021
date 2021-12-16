a017256c32413c716a125db18beebb770f87c138
Mathew Taylor <mat@mathewtaylor.co.uk>
2021-12-16 00:06:38 +0000

    Day 7, part 2.

--- pydays/day7/solution.py
+++ pydays/day7/solution.py
@@ -1,15 +1,15 @@
 import collections
-from typing import Iterable
+from typing import Callable, Iterable
 
 
-def get_cheapest_position(crabs: Iterable[int]):
+def get_cheapest_position(crabs: Iterable[int], fuel_calculator: Callable[[int], int]):
     max_position = max(crabs)
     min_position = min(crabs)
 
     total_fuel_per_position = collections.defaultdict(int)
     for crab in crabs:
         for pos in range(min_position, max_position):
-            total_fuel_per_position[pos] += abs(crab - pos)
+            total_fuel_per_position[pos] += fuel_calculator(crab - pos)
 
     position = min(total_fuel_per_position, key=total_fuel_per_position.get)
     return position, total_fuel_per_position[position]
@@ -19,6 +19,9 @@ if __name__ == "__main__":
     with open("input.txt") as f:
         crabs = [int(c) for c in f.read().strip().split(",")]
 
-    position, fuel = get_cheapest_position(crabs)
+    position, fuel = get_cheapest_position(crabs, fuel_calculator=lambda x: abs(x))
 
+    print(f"Part One; position: {position} fuel: {fuel}")
+
+    position, fuel = get_cheapest_position(crabs, fuel_calculator=lambda x: sum(range(1, abs(x) + 1)))
     print(f"Part One; position: {position} fuel: {fuel}")
--- pydays/day7/test_solution.py
+++ pydays/day7/test_solution.py
@@ -4,4 +4,8 @@ CRABS = (16, 1, 2, 0, 4, 2, 7, 1, 2, 14)
 
 
 def test_cheapest_positioning():
-    assert get_cheapest_position(CRABS) == (2, 37)
+    assert get_cheapest_position(CRABS, lambda x: abs(x)) == (2, 37)
+
+
+def test_cheapest_positioning_expensive_fuel():
+    assert get_cheapest_position(CRABS, lambda x: sum(range(1, abs(x) + 1))) == (5, 168)
